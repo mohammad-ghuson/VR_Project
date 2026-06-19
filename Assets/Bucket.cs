787e7d1b5aa9feb8a6f7d8f174171c31eae9fba0
ghuson <mohammad.ghuson2002@gmail.com>
2026-06-19 09:31:57 +0300

    Add circular motion option to bucket simulation

--- Assets/Bucket.cs
+++ Assets/Bucket.cs
@@ -9,6 +9,8 @@ public class Bucket : MonoBehaviour
     float time = 0;
     public float dt = 0.01f;
 
+    public bool useCircularMotion = false;
+
     float toRad(float d)
     {
         return (d / 180) * (Mathf.PI);
@@ -19,17 +21,41 @@ public class Bucket : MonoBehaviour
         pos0 = transform.position;
     }
 
+    //void Update()
+    //{
+
+    //  time += dt;
+
+    //float thetaMaxRad = toRad(thetaMax);
+
+    //float thetaRad = thetaMaxRad * Mathf.Cos(omega * time);
+    //pos.x = l * Mathf.Sin(thetaRad);
+    //pos.y = -l * Mathf.Cos(thetaRad);
+    //this.transform.position = pos + pos0;
+
+    //}
+
     void Update()
     {
-
         time += dt;
 
-        float thetaMaxRad = toRad(thetaMax);
+        if (useCircularMotion)
+        {
+            float theta = omega * time;
 
-        float thetaRad = thetaMaxRad * Mathf.Cos(omega * time);
-        pos.x = l * Mathf.Sin(thetaRad);
-        pos.y = -l * Mathf.Cos(thetaRad);
+            pos.x = l * Mathf.Cos(theta);
+            pos.z = l * Mathf.Sin(theta);
+        }
+        else
+        {
+            float thetaMaxRad = toRad(thetaMax);
+
+            float thetaRad = thetaMaxRad * Mathf.Cos(omega * time);
+
+            pos.x = l * Mathf.Sin(thetaRad);
+            pos.y = -l * Mathf.Cos(thetaRad);
+        }
+
         this.transform.position = pos + pos0;
-
     }
 }
